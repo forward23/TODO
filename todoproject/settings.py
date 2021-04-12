133055b310e458479301204424ee6607453fb696
Ilya Moiseev <moiseev@dataduck.com>
2021-04-12 12:33:49 +0300

    secure_proxy_ssl_header and allowed_hosts update

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -178,6 +178,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True  # new
     SESSION_COOKIE_SECURE = True  # new
     CSRF_COOKIE_SECURE = True  # new
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Heroku
 import dj_database_url
